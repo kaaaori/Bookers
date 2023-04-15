849fe427a75a4c6cd9f79b16f2d5a96b1cc0e302
EC2 Default User <y.kor4562xoxo@gmail.com>
2023-04-15 09:07:15 +0000

    [add]css実装まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
   def create
-    book = Book.new(book_params)
-    book.save
-    flash[:notice] = "Book was successfully created."
-    redirect_to book_path(book.id)
-    
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      render 'books/index'#"indexではダメなのか？"
+    end
   end
 
   def index
@@ -21,16 +24,20 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully update."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully update."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to "/books"
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
   end
   
   private
